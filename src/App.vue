1839d143cb2f6f3d985d6cccdf04d2feb8a07568
liuxiaoxiao <1512755884@qq.com>
2020-09-22 18:51:09 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<!--<img alt="Vue logo" src="./assets/logo.png" />-->
+<My />
+</template>
+
+<script>
+import My from './components/My.vue'
+
+export default {
+  name: 'App',
+  components: {
+    My
+  }
+}
+</script>
---
+++ src/components/My.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+  美好的一天
+</div>
+</template>
