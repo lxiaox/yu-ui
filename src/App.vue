854a2dca2fd5d81dffaa24e2a63b81bf7f592087
liuxiaoxiao <1512755884@qq.com>
2020-09-26 16:08:27 +0800

    路由切换后关闭aside

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,15 @@ import {
   provide,
   ref
 } from 'vue';
+import router from './router';
 export default {
   name: "App",
   setup() {
     const asideVisible = ref(false)
     provide('asideVisible', asideVisible)
+    router.afterEach(() => {
+      asideVisible.value = false
+    })
   }
 };
 </script>
