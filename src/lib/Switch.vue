fc683ccc62d908480cab5cdf171434cbf536b75b
liuxiaoxiao <1512755884@qq.com>
2020-09-28 17:01:15 +0800

    switch css 1

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <button><span>switch组件</span></button>
+  <button><span></span></button>
 </div>
 </template>
 
@@ -11,5 +11,33 @@ export default {
 </script>
 
 <style lang="scss">
+$h1: 22px;
+$h2: $h1 - 4px;
 
+button {
+  width: $h1*2;
+  height: $h1;
+  background: gray;
+  border-radius: $h1/2;
+  position: relative;
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  >span {
+    width: $h2;
+    height: $h2;
+    border-radius: $h2/2;
+    background: #fff;
+    background: #111;
+    position: absolute;
+    top: 2px;
+    left: 2px;
+  }
+
+  &.checked>span {
+    left: calc(100% - #{$h2} - 2px)
+  }
+
+}
 </style>
