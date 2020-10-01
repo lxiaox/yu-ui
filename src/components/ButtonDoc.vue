9f74b2e8985dbeddb516579d149a7a53f29cc79c
liuxiaoxiao <1512755884@qq.com>
2020-10-01 14:45:26 +0800

    创建Button组件

--- src/components/ButtonDoc.vue
+++ src/components/ButtonDoc.vue
@@ -1,5 +1,14 @@
 <template>
 <div>
-  button
+  <Button>按钮</Button>
 </div>
 </template>
+
+<script>
+import Button from '../lib/Button.vue'
+export default {
+  components: {
+    Button
+  },
+}
+</script>
