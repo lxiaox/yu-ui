e7e28d3bb85ceca0b4e4353198d918ab2403cdb7
liuxiaoxiao <1512755884@qq.com>
2020-10-02 17:21:20 +0800

    button css 1

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-<button>
+<button class="yu-btn">
   <slot />
 </button>
 </template>
@@ -7,3 +7,28 @@
 <script lang="ts">
 export default {}
 </script>
+
+<style lang="scss">
+$blue: #57a1f1;
+
+.yu-btn {
+  padding: 0 14px;
+  height: 32px;
+  border-radius: 4px;
+  background: #fff;
+  outline: none;
+  border: 1px solid #d9d9d9;
+  color: #555;
+  font-weight: 400;
+  line-height: 14px;
+  white-space: nowrap;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+
+  &:hover {
+    color: $blue;
+    border-color: $blue;
+  }
+}
+</style>
