24a322df7b9f595d3373dd4d21fb93fe1179779c
liuxiaoxiao <1512755884@qq.com>
2020-10-08 15:19:39 +0800

    使用短横线命名法

--- src/components/SwitchDoc.vue
+++ src/components/SwitchDoc.vue
@@ -3,12 +3,12 @@
   <h1>Switch</h1>
   <Switch v-model:value="value1" />
   <h1>自定义颜色</h1>
-  <Switch v-model:value="value2" activeColor="#5fe676" inactiveColor="#ff4949" />
-  <Switch v-model:value="value3" activeColor="#5fe676" inactiveColor="#e6a23c" />
+  <Switch v-model:value="value2" active-color="#5fe676" inactive-color="#ff4949" />
+  <Switch v-model:value="value3" active-color="#5fe676" inactive-color="#e6a23c" />
   <h1>禁用状态</h1>
   <Switch v-model:value="value4" disabled />
   <Switch v-model:value="value5" disabled />
-  <Switch v-model:value="value6" activeColor="#5fe676" inactiveColor="#ff4949" disabled />
+  <Switch v-model:value="value6" active-color="#5fe676" inactive-color="#ff4949" disabled />
 </div>
 </template>
 
