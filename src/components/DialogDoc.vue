f4993356c97e927f6f2c8a6dd560208fd3d38d5c
liuxiaoxiao <1512755884@qq.com>
2020-10-09 16:47:05 +0800

    创建Dialog组件

--- src/components/DialogDoc.vue
+++ src/components/DialogDoc.vue
@@ -1,5 +1,34 @@
 <template>
 <div>
-  dialog
+  <Button @click="toggle">toggle</Button>
+  <Dialog :visible="visible"></Dialog>
 </div>
 </template>
+
+<script lang="ts">
+import {
+  ref
+} from 'vue'
+import Dialog from "../lib/Dialog.vue"
+import Button from "../lib/Button.vue"
+export default {
+  components: {
+    Dialog,
+    Button
+  },
+  setup() {
+    const visible = ref(false)
+    const toggle = () => {
+      visible.value = !visible.value
+    }
+    return {
+      visible,
+      toggle
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  
+</style>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,17 @@
+<template>
+<div v-show="visible">
+  Dialogkkkk
+</div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    visible: Boolean,
+  }
+}
+</script>
+
+<style lang="scss">
+  
+</style>
