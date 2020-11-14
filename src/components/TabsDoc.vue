de5ef5e7a6c9e65512137141b6c8d5cc497c54a8
liuxiaoxiao <1512755884@qq.com>
2020-11-14 13:48:19 +0800

    创建tabs&tab组件

--- src/components/TabsDoc.vue
+++ src/components/TabsDoc.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>tabs</div>
+  <h1>tabs示例</h1>
+  <Tabs>
+    <Tab title="标题1">
+      <div>1</div>
+    </Tab>
+    <Tab title="标题2">
+      <div>2</div>
+    </Tab>
+  </Tabs>
 </template>
+
+<script>
+import Tabs from '../lib/tabs.vue'
+import Tab from '../lib/tab.vue'
+export default {
+  components: {
+    Tabs,
+    Tab,
+  },
+}
+</script>
+<style lang="scss">
+h1 {
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/lib/tab.vue
@@ -0,0 +1,10 @@
+<template>
+<div>tab</div>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
+
+<style lang="scss">
+</style>
