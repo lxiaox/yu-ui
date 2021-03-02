e5ea2e12dc5c04d59e99177f21695d4ac1019512
liuxiaoxiao <1512755884@qq.com>
2021-03-02 14:27:03 +0800

    添加Doc页介绍路由

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,9 @@ import { createWebHashHistory, createRouter } from "vue-router";
 import Home from "./views/Home.vue";
 import Doc from "./views/Doc.vue";
 import DocInfo from "./components/DocInfo.vue";
+import Intro from "./components/Intro.vue";
+import Install from "./components/Install.vue";
+import GetStarted from "./components/GetStarted.vue";
 import SwitchDoc from "./components/SwitchDoc.vue";
 import ButtonDoc from "./components/ButtonDoc.vue";
 import DialogDoc from "./components/DialogDoc.vue";
@@ -16,6 +19,9 @@ const router = createRouter({
       component: Doc,
       children: [
         { path: "", component: DocInfo },
+        { path: "intro", component: Intro },
+        { path: "install", component: Install },
+        { path: "get-started", component: GetStarted },
         { path: "switch", component: SwitchDoc },
         { path: "button", component: ButtonDoc },
         { path: "dialog", component: DialogDoc },
