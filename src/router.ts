bee373316a34ec5a7e66d3e0c8bb7c9e3f78819b
liuxiaoxiao <1512755884@qq.com>
2021-03-03 21:05:39 +0800

    优化markdown2: 使用h函数删减vue文件

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,12 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 import Home from "./views/Home.vue";
 import Doc from "./views/Doc.vue";
-import Intro from "./components/Intro.vue";
-import Install from "./components/Install.vue";
-import GetStarted from "./components/GetStarted.vue";
 import SwitchDoc from "./components/SwitchDoc.vue";
 import ButtonDoc from "./components/ButtonDoc.vue";
 import DialogDoc from "./components/DialogDoc.vue";
 import TabsDoc from "./components/TabsDoc.vue";
+import Markdown from "./components/Markdown.vue"
+import{h} from 'vue'
 const history = createWebHashHistory();
 const router = createRouter({
   history,
@@ -18,9 +17,9 @@ const router = createRouter({
       component: Doc,
       children: [
         { path: "", redirect: "/doc/intro" },
-        { path: "intro", component: Intro },
-        { path: "install", component: Install },
-        { path: "get-started", component: GetStarted },
+        { path: "intro", component: h(Markdown,{path:'../markdown/intro.md',key:'intro'}) },
+        { path: "install", component: h(Markdown,{path:'../markdown/install.md',key:'install'}) },
+        { path: "get-started", component: h(Markdown,{path:'../markdown/getStarted.md',key:'getStarted'}) },
         { path: "switch", component: SwitchDoc },
         { path: "button", component: ButtonDoc },
         { path: "dialog", component: DialogDoc },
