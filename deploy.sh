5fe8ab4d965fdf9dccd5ace482e31bfa0d977743
liuxiaoxiao <1512755884@qq.com>
2021-03-10 12:50:51 +0800

    部署到码云

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -m main &&
-git remote add origin git@github.com:lxiaox/YU-UI-Website.git &&
+git remote add origin git@gitee.com:liuxiaoxiao21/yu-ui-website.git &&
 git push -f -u origin main &&
 cd -
