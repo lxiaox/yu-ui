43ef841f7103303f1970bcc7313fcd5ae4c9a4f1
liuxiaoxiao <1512755884@qq.com>
2022-03-12 20:29:55 +0800

    vite配置：修改获取组件源代码方式，添加注释

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,29 @@
-import {md} from './plugins/md';
+// vite 本地服务/打包配置
+import { md } from './plugins/md'
 import fs from 'fs'
-import {baseParse} from '@vue/compiler-core'
 export default {
   assetsDir: 'assets',
-  base:'./',
-  plugins: [md()],
+  base: './',
+  plugins: [md()], // 展示md介绍文字的插件，
   vueCustomBlockTransforms: {
-    demo: (options) => {
-      const { code, path } = options
-      const file = fs.readFileSync(path).toString()
-      const parsed = baseParse(file).children.find(n => (n as any).tag === 'demo')
-      const title = (parsed as any).children[0].content
-      const main = file.split(parsed.loc.source).join('').trim()
+    demo: ({ code, path }) => {
+      // code：是<demo>xxx</demo>中间的xxx，path：文件路径，
+      // 调试方法：console.log(code)在终端/命令行查看
+      const file = fs.readFileSync(path).toString() // 文件内容字符串
+      const main = file
+        .split(`<demo>${code}</demo>`)
+        .join('')
+        .trim()
+        .replace(/\.\.\/\.\.\/lib\/index/, 'yuuyui')
+      // split()：把<demo>...</demo>这一段去除，得到：['','后面内容']，
+      // join()：拼接起来，
+      // trim()：去除空白字符，
+      // replace()：将引入路径 '../../lib/index' 替换为 'yuuyui'
+
       return `export default function (Component) {
-        Component.__sourceCode = ${
-        JSON.stringify(main)
-        }
-        Component.__sourceCodeTitle = ${JSON.stringify(title)}
+        Component.__sourceCode = ${JSON.stringify(main)}
+        Component.__sourceCodeTitle = ${JSON.stringify(code)}
       }`.trim()
-    }
-  }
+    },
+  },
 }
