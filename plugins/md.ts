979b0da61f878d499b032cdc6baf3d765a39fe87
liuxiaoxiao <1512755884@qq.com>
2022-03-12 20:33:05 +0800

    md文件转js的插件：添加注释

--- plugins/md.ts
+++ plugins/md.ts
@@ -3,16 +3,19 @@ import path from 'path'
 import fs from 'fs'
 import marked from 'marked'
 
-const mdToJs = str => {
+const mdToJs = (str) => {
   const content = JSON.stringify(marked(str))
+  // 把markdown语法转换为html语法，如 marked('# 一级标题') // <h1 id="一级标题">一级标题</h1>
   return `export default ${content}`
 }
 
 export function md() {
   return {
-    configureServer: [ // 用于开发
+    configureServer: [
+      // 用于开发
       async ({ app }) => {
-        app.use(async (ctx, next) => { // koa
+        app.use(async (ctx, next) => {
+          // koa
           if (ctx.path.endsWith('.md')) {
             ctx.type = 'js'
             const filePath = path.join(process.cwd(), ctx.path)
@@ -23,9 +26,12 @@ export function md() {
         })
       },
     ],
-    transforms: [{  // 用于 rollup // 插件
-      test: context => context.path.endsWith('.md'),
-      transform: ({ code }) => mdToJs(code) 
-    }]
+    transforms: [
+      {
+        // 用于 rollup // 插件
+        test: (context) => context.path.endsWith('.md'),
+        transform: ({ code }) => mdToJs(code),
+      },
+    ],
   }
 }
