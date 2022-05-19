3566525bbc1e9a79a69c8a62ddf6d25655eddc37
liuxiaoxiao <1512755884@qq.com>
2022-05-19 22:49:29 +0800

    格式化

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { createApp } from "vue";
-import "./lib/yu.scss";
-import App from "./App.vue";
-import "./index.scss";
-import router from "./router";
-import "github-markdown-css"
+import { createApp } from 'vue'
+import './lib/yu.scss'
+import App from './App.vue'
+import './index.scss'
+import router from './router'
+import 'github-markdown-css'
 import Demo from './components/Demo.vue'
 
-const app = createApp(App);
-app.use(router);
-app.mount("#app");
-app.component('Demo',Demo)
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
+app.component('Demo', Demo)
