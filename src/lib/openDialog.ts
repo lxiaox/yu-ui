500a5416441f375f4d353b2438620d628d842eec
liuxiaoxiao <1512755884@qq.com>
2022-05-19 22:51:25 +0800

    格式化

--- src/lib/openDialog.ts
+++ src/lib/openDialog.ts
@@ -2,29 +2,56 @@ import { createApp, h } from 'vue'
 import Dialog from './Dialog.vue'
 export const openDialog = (options) => {
   // 获取参数
-  const { ok, cancel, overlayClosable, closable, titleText, okText, cancelText, headerVisible, footerVisible, title, content, footer } = options
+  const {
+    ok,
+    cancel,
+    overlayClosable,
+    closable,
+    titleText,
+    okText,
+    cancelText,
+    headerVisible,
+    footerVisible,
+    title,
+    content,
+    footer,
+  } = options
   // 注销Dialog函数
   const close = () => {
     app.unmount()
     div.remove()
   }
-  // 把dib添加到body上
+  // 把div添加到body上
   const div = document.createElement('div')
   document.body.appendChild(div)
   const app = createApp({
     render() {
-      return h(Dialog, {
-        visible: true,
-        'onUpdate:visible': (newVisible) => {
-          if (newVisible === false) {
-            close()
-          }
+      return h(
+        Dialog,
+        {
+          visible: true,
+          'onUpdate:visible': (newVisible) => {
+            if (newVisible === false) {
+              close()
+            }
+          },
+          ok,
+          cancel,
+          overlayClosable,
+          closable,
+          titleText,
+          okText,
+          cancelText,
+          headerVisible,
+          footerVisible,
         },
-        ok, cancel, overlayClosable, closable, titleText, okText, cancelText, headerVisible, footerVisible,
-      }, {
-        title, default: content, footer,
-      })
-    }
+        {
+          title,
+          default: content,
+          footer,
+        }
+      )
+    },
   })
   app.mount(div)
 }
